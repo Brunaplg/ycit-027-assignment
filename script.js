ba45d5ad4bfbf593a8649e706a05412fdae34f17
Brunaplg <brunapelege@outlook.com>
2022-12-01 15:04:45 -0500

    Changes to complete the Assignment

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
+
+
 function getCatImage() {
-    fetch("https://api.thecatapi.com/v1/images/search")
+    return fetch("https://api.thecatapi.com/v1/images/search")
         .then((response) => {
-            // Resolve the promise we got from calling then() to the promise that we get from calling json()
-            response.json();
+            return response.json()
         })
         .then((data) => {
-            console.log(data[0].url);
-            document.getElementById("catImage").src = data[0].url;
-        });
+
+            document.getElementById("catImage").src = data[0].url
+        })
+}
+
+function getCatImage2() {
+    return fetch("https://api.thecatapi.com/v1/images/search")
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+
+            document.getElementById("catImage2").src = data[0].url
+        })
+}
+
+function getCatImage3() {
+    return fetch("https://api.thecatapi.com/v1/images/search")
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+
+            document.getElementById("catImage3").src = data[0].url
+        })
+}
+function getAllCatImages() {
+
+    return Promise.all([getCatImage(), getCatImage2(), getCatImage3()])
+
+        .then((data) => {
+            document.getElementById("catImage").src = data[0].url
+            document.getElementById("catImage2").src = data[0].url
+            document.getElementById("catImage3").src = data[0].url
+        })
 }
